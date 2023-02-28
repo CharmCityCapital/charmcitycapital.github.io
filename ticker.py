7eb4d436d532e89a884c1262d15011a9495fddf3
Charm City Capital <116847696+CharmCityCapital@users.noreply.github.com>
2023-02-28 14:03:53 -0500

    Update ticker.py

--- ticker.py
+++ ticker.py
@@ -1,23 +1,14 @@
-from flask import Flask
 import yfinance as yf
+import json
 
-app = Flask(__name__)
+symbols = ['TROW', 'MKC', 'UA', 'ENOV', 'LAUR']
+data = yf.download(symbols, start="2023-02-25", end="2023-02-25")
+prices = data['Close'].iloc[-1]
+stock_data = " | ".join([f"{symbol}: {price:.2f}" for symbol, price in prices.items()])
 
-@app.route("/ticker")
-def ticker():
-    # Fetch stock data using yfinance
-    symbols = ['TROW', 'MKC', 'UA', 'ENOV', 'LAUR']
-    data = yf.download(symbols, {module: 'quote', interval: '1d', period: '1d'})
-    prices = data['quote']['regularMarketPrice']
-    stockData = " | ".join(f"{symbol}: {price}" for symbol, price in prices.items())
+response = requests.get("https://api.simpleswap.io/v1/ticker")
+data = json.loads(response.text)
+cryptos = ["BTC", "ETH", "LTC", "XMR", "DOGE"]
+crypto_data = " | ".join([f"{crypto}: {data[crypto.upper()]['rate']:.2f}" for crypto in cryptos])
 
-    # Fetch crypto data from SimpleSwap API
-    response = requests.get("https://api.simpleswap.io/v1/ticker")
-    data = response.json()
-    cryptos = ["BTC", "ETH", "LTC", "XMR", "DOGE"]
-    cryptoData = " | ".join(f"{crypto}: {data[crypto.upper()]['rate']}" for crypto in cryptos)
-
-    return f"{stockData}|{cryptoData}"
-
-if __name__ == "__main__":
-    app.run()
+print(f"{stock_data}|{crypto_data}")
